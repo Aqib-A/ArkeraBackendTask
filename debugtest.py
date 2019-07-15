ab9e7a2f024214245b4d9ff6ac539170ddee93f7
Aqib <aqibabbas97@gmail.com>
2019-07-15 12:12:14 +0100

    checked

--- debugtest.py
+++ debugtest.py
@@ -11,7 +11,7 @@ def increment_dictionary_values(d, i):
 class TestIncrementDictionaryValues(TestCase):
     def test_increment_dictionary_values(self):
         d = {'a': 1}
-        dd = increment_dictionary_values(d,1)
+        dd = increment_dictionary_values(d, 1)
         ddd = increment_dictionary_values(d, -1)
         self.assertEqual(dd['a'], 2)
         self.assertEqual(ddd['a'], 0)
